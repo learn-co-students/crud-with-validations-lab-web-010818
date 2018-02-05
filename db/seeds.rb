bff2ab6c1ce011a9e311355833c5be90a6436ade
cnlucke <cristy.lucke@gmail.com>
2018-02-05 18:08:44 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Song.find_or_create_by( title: "Hello",
+  released: true,
+  release_year: 2012,
+  artist_name: "Adele",
+  genre: "pop")
+Song.find_or_create_by( title: "Rolling in the Deep",
+  released: true,
+  release_year: 2011,
+  artist_name: "Adele",
+  genre: "pop")
+Song.find_or_create_by( title: "Like A Prayer",
+  released: true,
+  release_year: 1986,
+  artist_name: "Madonna",
+  genre: "pop")
